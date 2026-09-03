4e3adb3b8c77ed04fe01c398804c5081867d5c06
onedayrolling18times <1186540691@qq.com>
2026-09-03 15:31:21 +0800

    Delete sw.js

--- sw.js
+++
@@ -1,6 +0,0 @@
-const CACHE='sop-v2';
-const SHELL=['./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
-
-self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(SHELL)).then(()=>self.skipWaiting()))});
-self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
-self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(res=>{if(res.ok){const clone=res.clone();caches.open(CACHE).then(c=>c.put(e.request,clone));}return res}).catch(()=>{if(e.request.mode==='navigate')return caches.match('./index.html');return new Response('',{status:408})})))})
